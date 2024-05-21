363798992be7b11d137254aa3e6e5b7adce355a5
Gunnar Þórunnarson <gus@tskoli.is>
2024-05-21 17:30:41 +0000

    Add files via upload

---
+++ kodi/reed.ino.ino
@@ -0,0 +1,22 @@
+int REED_PIN = 7;	// Pin connected to reed switch
+int LED_PIN = 2;	// LED pin
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(REED_PIN, INPUT_PULLUP);	// Enable internal pull-up for the reed switch
+	pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+	int proximity = digitalRead(REED_PIN); // Read the state of the switch
+	
+	// If the pin reads low, the switch is closed.
+	if (proximity == LOW) {
+		Serial.println("Switch closed");
+		digitalWrite(LED_PIN, HIGH);	// Turn the LED on
+	}
+	else {
+		Serial.println("Switch opened");
+		digitalWrite(LED_PIN, LOW);		// Turn the LED off
+	}
+}
