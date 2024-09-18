438b90fc7b39751a828f75f0d366137303309ae2
Gunnar Þórunnarson <gus@tskoli.is>
2024-09-18 11:42:58 +0000

    Update takki.py

--- MicorPython/takki.py
+++ MicorPython/takki.py
@@ -1,7 +1,11 @@
+# Takki, prentar út 0 í Shell ef ýtt er á takka annars 1
+# Tengingar og kóði: https://wokwi.com/projects/409364827730921473
+
 from machine import Pin
-takki = Pin(14, Pin.IN, Pin.PULL_UP)   	# Segjum að pinninn sé inntakspinni, vír tengdur í GND. 
+takki = Pin(2, Pin.IN, Pin.PULL_UP)   # Segjum að pinninn sé inntakspinni, vír tengdur í GND. 
 
 while True:
+
     # ef ekki er ýtt á takka
     print(takki.value()) 		# skrifar út 1 í Shell (upphafsstaða)
   
