e615801fd9f494a1cd5462354b26037c7c44a1d0
Gunnar Þórunnarson <gus@tskoli.is>
2024-12-17 15:33:29 +0000

    Update takki.py

--- MicroPython/takki.py
+++ MicroPython/takki.py
@@ -1,16 +1,15 @@
 # Takki, prentar út 0 í Shell ef ýtt er á takka annars 1
 # Tengingar og kóði: https://wokwi.com/projects/409364827730921473
-
 from machine import Pin
 takki = Pin(2, Pin.IN, Pin.PULL_UP)   # Segjum að pinninn sé inntakspinni, vír tengdur í GND. 
 
 while True:
-
-    # ef ekki er ýtt á takka
-    print(takki.value()) 		# skrifar út 1 í Shell (upphafsstaða)
-  
-    # ef ýtt er á takka 
-    if not takki.value():        # ef ekki 1     
-        print(takki.value())     # skrifar út 0 í Shell
+ 
+    print(ekki búið að ýta á takka) 		
+    
+    if takki.value() == 0:      # ef ýtt er á takka, ekki 1     
+        print(takki.value())    # skrifar út 0 í Shell
+    
+    print("takka sleppt") 		# búið að sleppa takka eða ekki ýtt á takka 
     
 # ath. value() les mörgum sinnum á meðan haldið er takka inni.
