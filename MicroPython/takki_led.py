f5db1d69288752bb043e71f38b63e3d692484962
Gunnar Þórunnarson <gus@tskoli.is>
2025-01-15 13:15:21 +0000

    Update takki_led.py

--- MicroPython/takki_led.py
+++ MicroPython/takki_led.py
@@ -7,9 +7,10 @@ led = Pin(4, Pin.OUT)
 takki = Pin(2, Pin.IN, Pin.PULL_UP)	 # vír tengdur í GND, upphafsstaða á takka er 1  (ekki ýtt á takka)
 
 while True:
-    if takki.value() == 0:  # ef ýtt er á takka (1 verður 0). 
+    # ef ýtt er á takka (1 verður 0). 
+    if takki.value() == 0:  # ef ýtt er á takka þá er gildið 0 lesið af takka með value() aðferð. 
         led.value(1)        # led on
     else:
         led.value(0)        # led off (ekki ýtt á takka eða takka sleppt) 
         
-Ath. value() les mörgum sinnum á meðan haldið er takka inni.
+
