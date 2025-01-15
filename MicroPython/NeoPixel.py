d9f6e955bc83560565fb0337837defb9c0f70d57
Gunnar Þórunnarson <gus@tskoli.is>
2025-01-15 13:33:02 +0000

    Update NeoPixel.py

--- MicroPython/NeoPixel.py
+++ MicroPython/NeoPixel.py
@@ -19,7 +19,7 @@ off   =  [ 0, 0, 0]             # ekkert ljós
 
 while True:
     # stjórnum öllum 8 leds í einu með að nota fill
-    neo.fill([0, 0, 0])
+    np.fill([0, 0, 0])
     
     # led 1  
     np[0] = red		        # eða [255, 0, 0] 
